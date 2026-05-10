fb6ec39a3767000d3aad53054c917eac40ae9ca8
Kelfi Batista <kelfi.bm@gmail.com>
2026-05-09 23:41:56 -0400

    feat: adding ConfigModule

--- ms-products/src/app.module.ts
+++ ms-products/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from '@products/products.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ConfigModule.forRoot(), ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
